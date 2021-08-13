c58c82b7b4430e86cc28aafa58879515f2c1edea
AQUINRU <russel.aquino@oocl.com>
2021-08-13 20:35:49 +0800

    [CompanyIntegrationTest][Russel] test: add should_add_company_when_call_add_company

--- src/test/java/com/thoughtworks/springbootemployee/integration/CompanyIntegrationTest.java
+++ src/test/java/com/thoughtworks/springbootemployee/integration/CompanyIntegrationTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
@@ -41,5 +42,35 @@ public class CompanyIntegrationTest {
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$[0].id").isNumber())
                 .andExpect(jsonPath("$[0].companyName").value(company.getCompanyName()));
+    }
+
+    @Test
+    public void should_add_company_when_call_add_company() throws Exception {
+        // Given
+        String stringAsJson = "{\n" +
+                "    \"companyName\" : \"OOCL\",\n" +
+                "    \"employees\" : [\n" +
+                "        {\n" +
+                "            \"name\" : \"russ\",\n" +
+                "            \"age\" : 22,\n" +
+                "            \"gender\" : \"male\",\n" +
+                "            \"salary\" : 1000\n" +
+                "        }\n" +
+                "    ]\n" +
+                "}";
+
+        // When & Then
+        mockMvc.perform(MockMvcRequestBuilders.post("/companies")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(stringAsJson))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").isNumber())
+                .andExpect(jsonPath("$.companyName").value("OOCL"))
+                .andExpect(jsonPath("$.employees").isNotEmpty())
+                .andExpect(jsonPath("$.employees[0].id").isNumber())
+                .andExpect(jsonPath("$.employees[0].name").value("russ"))
+                .andExpect(jsonPath("$.employees[0].age").value(22))
+                .andExpect(jsonPath("$.employees[0].gender").value("male"))
+                .andExpect(jsonPath("$.employees[0].salary").value(1000));
     }
 }
